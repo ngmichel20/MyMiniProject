6312963fd0f652d82f04a1c430383c1e6d0c7dad
ngmichel20 <ngmichel20@hotmail.com>
2021-12-01 17:56:33 +0100

    Initial commit with dialect SQLite

---
+++ src/main/java/com/myminiproject/demo/MyMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myminiproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMiniProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyMiniProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/myminiproject/demo/MyMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myminiproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMiniProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
