defde78935db58374c4df75e3ed8fcf51189fad0
ngmichel20 <ngmichel20@hotmail.com>
2021-12-03 21:59:03 +0100

    Adding CustomerRepository interface in order to have the jpa repository

--- src/main/java/com/myminiproject/demo/repository/CustomerRepository.java
+++ src/main/java/com/myminiproject/demo/repository/CustomerRepository.java
@@ -1,5 +1,10 @@
 package com.myminiproject.demo.repository;
 
-public class CustomerRepository {
+import com.myminiproject.demo.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
